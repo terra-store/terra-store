bc48adda6ba911d64eb7e5b6bcfe98440d82c9ed
Jamie <jamie@jduncombe.com>
2021-05-10 20:54:36 +1000

    restructure api to better support future implementations of provider registry

--- terraform_registry_api/registry.py
+++ terraform_registry_api/registry.py
@@ -6,14 +6,13 @@ import json
 app = connexion.App(__name__, specification_dir="./")
 
 # Read the swagger.yml file to configure the endpoints
-app.add_api("swagger.yml")
+app.add_api("terraform_module_registry_api/swagger.yml")
 
 @app.route("/.well-known/terraform.json")
 def service_discovery():
     services = {
         "modules.v1": "http://localhost:5000/v1/modules",
-        "providers.v1": "http://localhost:5000/v1/providers",
-        "login.v1": "http://localhost:5000/v1/login"
+        "providers.v1": "http://localhost:5000/v1/providers"
     }
     return json.dumps(services)
 
