dab3cff2f3251fef7d7a2288df0895a19c644982
Jamie <jamie@jduncombe.com>
2021-05-11 06:30:55 +1000

    add number of basic tests and refactored
    In order to get some tests working a small amount of refactoring was performed.
    some dummy data and error handling added to the backend and api

---
+++ tests/test_registry.py
@@ -0,0 +1,22 @@
+import json
+import pytest
+
+from terraform_registry_api import registry
+
+
+@pytest.fixture
+def client():
+    app = registry.create_app()
+    app.app.testing = True
+    yield app.app.test_client() 
+
+def test_service_discovery_endpoint(client):
+    """Start with a blank database."""
+    services = {
+        "modules.v1": "http://localhost:5000/v1/modules",
+        "providers.v1": "http://localhost:5000/v1/providers"
+    }
+    rv = client.get('/.well-known/terraform.json')
+    assert rv.status_code == 200
+    print(rv.data)
+    assert json.loads(rv.data) == services
