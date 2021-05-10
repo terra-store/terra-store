34eb5bd5f6d06cfed55793782a36fc7d779be0e3
Jamie <jamie@jduncombe.com>
2021-05-11 06:30:55 +1000

    add number of basic tests and refactored
    In order to get some tests working a small amount of refactoring was performed.
    some dummy data and error handling added to the backend and api

--- terraform_registry_api/terraform_module_registry_api/api.py
+++ terraform_registry_api/terraform_module_registry_api/api.py
@@ -1,10 +1,17 @@
-from terraform_module_registry_api.backends import dummy as backend
+from terraform_registry_api.terraform_module_registry_api.backends import dummy as backend
+from terraform_registry_api.terraform_module_registry_api.exceptions import ModuleNotFoundException
 from flask import make_response
 
 def list_versions(namespace, name, provider):
-   return backend.get_versions(namespace, name, provider)
+   try:
+      return backend.get_versions(namespace, name, provider)
+   except ModuleNotFoundException as e:
+      return make_response(e.message, 404)
 
 def download_version(namespace, name, provider, version):
-    resp = make_response('', 204)
-    resp.headers['X-Terraform-Get'] = backend.download_version(namespace, name, provider, version)
-    return resp
+   try:
+      resp = make_response('', 204)
+      resp.headers['X-Terraform-Get'] = backend.download_version(namespace, name, provider, version)
+      return resp
+   except ModuleNotFoundException as e:
+      return make_response(e.message, 404)
