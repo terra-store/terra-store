85086590e0c7449762cf986924fd7eb04abd575b
Jamie <jamie@jduncombe.com>
2021-05-10 20:41:04 +1000

    implement basic api
    basic response types, with no error handling

--- terraform_module_registry_api/api.py
+++ terraform_module_registry_api/api.py
@@ -1,7 +1,10 @@
 from backends import dummy as backend
+from flask import make_response
 
 def list_versions(namespace, name, provider):
-    return backend.get_versions(namespace, name, provider)
+   return backend.get_versions(namespace, name, provider)
 
 def download_version(namespace, name, provider, version):
-    return ""
+    resp = make_response('', 204)
+    resp.headers['X-Terraform-Get'] = backend.download_version(namespace, name, provider, version)
+    return resp
