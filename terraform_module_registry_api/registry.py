711e95058dc8cbe88b58a557153b7a219c31da23
Jamie <jamie@jduncombe.com>
2021-05-10 19:59:37 +1000

    update registry and implement service dscovery

--- terraform_module_registry_api/registry.py
+++ terraform_module_registry_api/registry.py
@@ -1,5 +1,6 @@
 from flask import render_template
 import connexion
+import json
 
 # Create the application instance
 app = connexion.App(__name__, specification_dir="./")
@@ -7,9 +8,14 @@ app = connexion.App(__name__, specification_dir="./")
 # Read the swagger.yml file to configure the endpoints
 app.add_api("swagger.yml")
 
-# @app.errorhandler(404)
-# def page_not_found(e):
-#     return "{\"error\": \"Module Not Found\"}", 404
+@app.route("/.well-known/terraform.json")
+def service_discovery():
+    services = {
+        "modules.v1": "http://localhost:5000/v1/modules",
+        "providers.v1": "http://localhost:5000/v1/providers",
+        "login.v1": "http://localhost:5000/v1/login"
+    }
+    return json.dumps(services)
 
 if __name__ == "__main__":
     app.run(debug=True)
