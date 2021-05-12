e4ed6dce0e4646e8a1e69ba82eb2860e26b6f437
Jamie <jamie@jduncombe.com>
2021-05-12 23:46:34 +1000

    add docstrings to dummy backend

--- terraform_registry_api/terraform_module_registry_api/backends/dummy.py
+++ terraform_registry_api/terraform_module_registry_api/backends/dummy.py
@@ -16,7 +16,19 @@ dummy_data = {
 }
 
 def get_versions(namespace, name, provider):
-    
+    """Get The Versions
+
+    Args:
+        namespace (str): [description]
+        name (str): [description]
+        provider (str): [description]
+
+    Raises:
+        ModuleNotFoundException: [description]
+
+    Returns:
+        json: [description]
+    """
     module_name="/{namespace}/{name}/{provider}".format(namespace=namespace, name=name, provider=provider)
     if module_name in dummy_data['modules'].keys():
         list_versions= []
@@ -29,15 +41,28 @@ def get_versions(namespace, name, provider):
                 }
             ]
         }
-
         return json.dumps(versions)
     else:
         raise ModuleNotFoundException("Module Not Found: "+ module_name)
 
 def download_version(namespace, name, provider, version):
+    """Generate Download URL for module version
+
+    Args:
+        namespace (str): [description]
+        name (str): [description]
+        provider (str): [description]
+        version (str): [description]
+
+    Raises:
+        ModuleNotFoundException: [description]
+
+    Returns:
+        str: [description]
+    """
     module_name="/{namespace}/{name}/{provider}".format(namespace=namespace, name=name, provider=provider)
     if module_name in dummy_data['modules'].keys() and version in dummy_data['modules'][module_name]['versions']:
-        return "https://api.github.com/repos/{namespace}/terraform-{provider}-{name}/tarball/v{version}//*?archive=tar.gz".format(provider=provider, 
+        return "https://api.github.com/repos/{namespace}/terraform-{provider}-{name}/tarball/v{version}//*?archive=tar.gz".format(provider=provider,
                                                                                                                        name=name,
                                                                                                                        version=version,
                                                                                                                        namespace = namespace)
